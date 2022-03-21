586fd7317257876322834a34da423ef5b0ba1c04
eduardomunareto <eduardomunareto@hotmail.com>
2022-03-21 17:49:45 -0300

    Aula 264

---
+++ src/A0001-type-annotation/A0009-tipo-enum.ts
@@ -0,0 +1,22 @@
+//Estrutura de dados não ordenada, exemplo nome das cores, é um tipo e um valor
+enum Cores { //Cores tem que ser maisuculo, criou um objeto
+  VERMELHO = 10, //0
+  AZUL = 100, //1
+  AMARELO = 200, //2
+  ROXO = 300,
+}
+
+enum Cores { //Vai unir esses dois enum e um só, um complementa o outro
+  MARROM = 301,
+}
+console.log(Cores[10]);
+console.log(Cores.VERMELHO);
+console.log(Cores.AZUL);
+console.log(Cores.AMARELO);
+console.log(Cores.ROXO);
+
+function escolhaACor(cor: Cores): void {
+  console.log(Cores[cor]);
+}
+
+escolhaACor(Cores.MARROM);
