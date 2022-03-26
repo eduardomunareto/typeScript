8c022894c5a2af80280edb8742f8d96ea6671f24
eduardomunareto <eduardomunareto@hotmail.com>
2022-03-26 16:52:02 -0300

    Aula 268

---
+++ src/Type Annotation/A0014-type-alias.ts
@@ -0,0 +1,26 @@
+type Idade = number;
+type Pessoa = {
+  nome: string;
+  idade: Idade;
+  salario: number;
+  corPreferida?: CorPreferida;
+};
+
+type CorRGB = 'Vermelho' | 'Verde' | 'Azul';
+type CorCMYK = 'Ciano' | 'Magenta' | 'Amarelo' | 'Preto';
+type CorPreferida = CorRGB | CorCMYK;
+
+const pessoa2: Pessoa = {
+  nome: 'Eduardo',
+  idade: 35,
+  salario: 200_000,
+  corPreferida: 'Vermelho',
+};
+
+console.log(pessoa2);
+
+export function setCorPreferida(pessoa: Pessoa, cor: CorPreferida): Pessoa {
+  return { ...pessoa, corPreferida: cor };
+}
+
+console.log(setCorPreferida(pessoa2, 'Ciano'));
